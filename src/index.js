e7e014752881ffe53c3589d9c173a8b5e20dad0b
gvmertens <guilherme.mertens@totalcontroltelecom.com.br>
2020-03-18 20:15:05 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import Routes from './routes';
+// import { Container } from './styles';
+
+export default function src() {
+  return <Routes />;
+}
